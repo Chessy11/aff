740686046521b102597d37f9103025aa445cdc17
Zurab Skhiladze <zurabsx@gmail.com>
2022-10-13 01:51:28 +0400

    frontend

--- affiliate/settings.py
+++ affiliate/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--7@m&j%9eq&me%v)t(fykyqhpcr88rx+vydi#zu)@iu#1oq0w*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://powerful-basin-74576.herokuapp.com', 'localhost', '127.0.0.1']
 
