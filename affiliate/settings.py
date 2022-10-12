51f0fee91e6cc9e51cbee9354cada9db609ebf38
Zurab Skhiladze <zurabsx@gmail.com>
2022-10-12 19:47:56 +0400

    trusted origins

--- affiliate/settings.py
+++ affiliate/settings.py
@@ -148,6 +148,7 @@ STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static'),
 )
 
+CSRF_TRUSTED_ORIGINS = ["https://powerful-basin-74576.herokuapp.com/"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
