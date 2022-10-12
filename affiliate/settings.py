088fb412564b33fc3ed7522fff2b152b86ce4486
Zurab Skhiladze <zurabsx@gmail.com>
2022-10-12 23:32:26 +0400

    f touch

--- affiliate/settings.py
+++ affiliate/settings.py
@@ -149,9 +149,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+print("static_root", STATIC_ROOT)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static'),
 )
